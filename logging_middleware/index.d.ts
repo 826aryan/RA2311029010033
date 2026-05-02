fd75cb89b0791233a4f1669337c8d416ee216c8f
Aryan Raj <aryanraj@Aryans-MacBook-Air-93.local>
2026-05-02 11:22:53 +0530

    first commit

---
+++ logging_middleware/index.d.ts
@@ -0,0 +1,6 @@
+export type Stack = "backend" | "frontend";
+export type Level = "info" | "warn" | "error" | "fatal";
+export type FrontendPackage = "api" | "component" | "hook" | "page" | "state" | "style" | "auth" | "config" | "middleware" | "utils";
+export type BackendPackage = "cache" | "controller" | "cron job" | "db" | "domain" | "handler" | "repository" | "route" | "service" | "auth" | "config" | "middleware" | "utils";
+export type Package = FrontendPackage | BackendPackage;
+export declare function Log(stack: Stack, level: Level, pkg: Package, message: string): Promise<void>;
